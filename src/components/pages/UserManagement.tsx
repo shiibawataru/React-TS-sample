135c76c409b44a4bd3bd30716ab88bc96a413cc4
shiibawataru <93691047+shiibawataru@users.noreply.github.com>
2022-03-25 16:34:24 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManagement: VFC = memo(() => {
-  return <p>管理ページ</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ぽち"
+          fullName="Tama Pochi"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
